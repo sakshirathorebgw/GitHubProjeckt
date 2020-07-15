1bbe542b98888351cd2bcf6304df480611e6ca99
Sakshi Rathore <67410359+sakshirathorebgw@users.noreply.github.com>
2020-07-15 15:07:29 +0200

    Add files via upload

--- piecebypiece.sh
+++ piecebypiece.sh
@@ -14,24 +14,24 @@ SSHKEY_PATH=/c/Users/D35N/Downloads/GitHubProjeckt/Automation/newharbortestkey
   echo $BASENAME "is basename"
   TLS_NAME=$BASENAME
    echo $TLS_NAME "is new secret name for harbor"
-
+   
   #certificate password fetch
   PASSWORD_FILE=$(find ./ -type f -name "readme.txt" -printf "%T@ %p\n" | sort -n | cut -d' ' -f 2- | tail -n 1 )
   PASSWORD_VALUE=$(cat $PASSWORD_FILE)
   export PASSWORD_VALUE=$PASSWORD_VALUE
-  echo "this is password " $PASSWORD_VALUE
-
+  echo "this is password " $PASSWORD_VALUE 
+  
    #generate .crt and .key files
 /usr/bin/openssl pkcs12 -in $CERT_PATH -nodes -out $BASENAME.key  -password pass:$PASSWORD_VALUE
 /usr/bin/openssl pkcs12 -in $CERT_PATH -nokeys -out $BASENAME.crt  -password pass:$PASSWORD_VALUE
 
 # to get key and crt file path
 KEY_FILE=$(find ./ -type f -name "*key" -printf "%T@ %p\n" | sort -n | cut -d' ' -f 2- | tail -n 1)
-                export KEY_FILE=$KEY_FILE
-                        echo $KEY_FILE "key file generated"
+		export KEY_FILE=$KEY_FILE
+			echo $KEY_FILE "key file generated"
 CRT_FILE=$(find ./ -type f -name "*crt" -printf "%T@ %p\n" | sort -n | cut -d' ' -f 2- | tail -n 1)
-                export CRT_FILE=$CRT_FILE
-                        echo $CRT_FILE "crt file generated"
+		export CRT_FILE=$CRT_FILE
+			echo $CRT_FILE "crt file generated"
 # to get name for the key and crt file
 KEY_FILENAME=$(basename $KEY_FILE)
 echo "key filename is" $KEY_FILENAME
@@ -41,29 +41,29 @@ echo "crt filename is" $CRT_FILENAME
 CN_ADDRESS=$(openssl x509 -noout -subject -in $CRT_FILENAME | awk '{ print $18 }' | sed 's/.$//' 2>&1)
 echo "address for certificate is :" $CN_ADDRESS
 #declare variable for substitutions in values.yaml
-                SEARCH1="core: core.harbor.domain"
-                export SEARCH1=$SEARCH1
-                        echo $SEARCH1
+		SEARCH1="core: core.harbor.domain"
+		export SEARCH1=$SEARCH1
+			echo $SEARCH1
         REPLACE1="core: $CN_ADDRESS"
-                export REPLACE1=$REPLACE1
-                        echo $REPLACE1
+		export REPLACE1=$REPLACE1
+			echo $REPLACE1
 
  #sed -i "s/${SEARCH1}/${REPLACE1}/g" values.yaml
 
-                SEARCH2="notary: notary.harbor.domain"
-                export SEARCH2=$SEARCH2
-                        echo $SEARCH2
+		SEARCH2="notary: notary.harbor.domain"
+		export SEARCH2=$SEARCH2
+			echo $SEARCH2
         REPLACE2="notary: $CN_ADDRESS"
-                export REPLACE2=$REPLACE2
-                        echo $REPLACE2
+		export REPLACE2=$REPLACE2
+			echo $REPLACE2
 
  #sed -i "s/${SEARCH2}/${REPLACE2}/g" values.yaml
 
-                SEARCH3="secretName: "" "
-                export SEARCH3=$SEARCH3
+		SEARCH3="secretName: "" "
+		export SEARCH3=$SEARCH3
             echo $SEARCH3
         REPLACE3="secretName: $TLS_NAME"
-                export REPLACE3=$REPLACE3
+		export REPLACE3=$REPLACE3
             echo $REPLACE3
  #sed -i "s/secretName: ""/${REPLACE3}/g" values1.yaml
  #sed -i "s/secretName: ""/$REPLACE3/g" values.yaml
