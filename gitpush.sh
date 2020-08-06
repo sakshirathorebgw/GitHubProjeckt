0df60b3aeca08474ba0cd7e025e736c867ff588b
Sakshi Rathore <Sakshi.Rathore@bgw-online.de>
2020-08-06 07:21:21 +0000

    commiting the new file

--- gitpush.sh
+++ gitpush.sh
@@ -1,15 +1,15 @@
 git add .
 
 commitMessage="commiting the new file"
-echo 'Enter the commit message:'
+echo $commitMessage
 
-read commitMessage
+#read commitMessage
 
 git commit -m "$commitMessage"
 git remote set-url origin https://Sakshi.Rathore%40bgw-online.de:Sherlock%40107@github.com/sakshirathorebgw/GitHubProjeckt.git
 branch=master
-echo 'Enter the name of the branch:'
-read branch
+echo $branch
+#read branch
 
 git push origin $branch
 
