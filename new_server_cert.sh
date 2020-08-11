a716b4f0d13e5ff8a63b1c0dde2a4ce720ac7454
Sakshi Rathore <Sakshi.Rathore@bgw-online.de>
2020-08-11 09:17:20 +0000

    commiting the new file

--- new_server_cert.sh
+++ new_server_cert.sh
@@ -4,42 +4,71 @@ helm repo add harbor https://helm.goharbor.io
 helm fetch harbor/harbor --untar
 
  #to create namespace
-kubectl create namespace harbor
+#kubectl create namespace harbor
 
 cd harbor
 #to create secret 
 # var1= harbor
-
+cp ../*.pfx .
 cp ../*.crt .
 cp ../*.key .
-ls
- kubectl create secret $TLS_NAME --key $KEY_FILENAME --cert $CRT_FILENAME --namespace harbor
+ls -lrt
+ #to create namespace
+kubectl create namespace harbor
+
+ kubectl create secret tls $TLS_NAME --key $KEY_FILENAME --cert $CRT_FILENAME --namespace harbor
  
+ CERT_PATH=$(find ./ -type f -name "*pfx" -printf "%T@ %p\n" | sort -n | cut -d' ' -f 2- | tail -n 1 )
+ export CERT_PATH=$CERT_PATH
+ echo $CERT_PATH "is certpath"
+  BASENAME=$(basename $CERT_PATH .pfx)
+  export BASENAME=$BASENAME
+  echo $BASENAME "is basename"
+  TLS_NAME=$BASENAME
+   echo $TLS_NAME "is new secret name for harbor"
+
+KEY_FILE=$(find ./ -type f -name "*key" -printf "%T@ %p\n" | sort -n | cut -d' ' -f 2- | tail -n 1)
+                export KEY_FILE=$KEY_FILE
+                        echo $KEY_FILE "key file generated"
+CRT_FILE=$(find ./ -type f -name "*crt" -printf "%T@ %p\n" | sort -n | cut -d' ' -f 2- | tail -n 1)
+                export CRT_FILE=$CRT_FILE
+                        echo $CRT_FILE "crt file generated"
+# to get name for the key and crt file
+KEY_FILENAME=$(basename $KEY_FILE)
+echo "key filename is" $KEY_FILENAME
+CRT_FILENAME=$(basename $CRT_FILE)
+echo "crt filename is" $CRT_FILENAME
+
+
  CN_ADDRESS=$(openssl x509 -noout -subject -in $CRT_FILENAME | awk '{ print $18 }' | sed 's/.$//' 2>&1)
-        SEARCH1="core: core.harbor.domain"
-        echo $SEARCH1
+      #  SEARCH1="core: core.harbor.domain"
+      #  echo $SEARCH1
         REPLACE1="core: $CN_ADDRESS"
         echo $REPLACE1
+ sed -i "0,/ core: /{s/ core:.*/ ${REPLACE1}/}" values.yaml
 
  #sed -i "s/${SEARCH1}/${REPLACE1}/g" values.yaml
-        SEARCH2="notary: notary.harbor.domain"
-        echo $SEARCH2
+      # SEARCH2="notary: notary.harbor.domain"
+      # echo $SEARCH2
         REPLACE2="notary: $CN_ADDRESS"
          echo $REPLACE2
+ sed -i "0,/ notary: /{s/ notary:.*/ ${REPLACE2}/}" values.yaml
 
 # sed -i "s/${SEARCH2}/${REPLACE2}/g" values.yaml
-        SEARCH3="secretName: """
-		echo $SEARCH3
+       # SEARCH3="secretName: """
+	#	echo $SEARCH3
 		REPLACE3="secretName: $TLS_NAME"
 		echo $REPLACE3
+ sed -i "s/secretName:.*/${REPLACE3}/g" values.yaml
+
 # sed -i "s/${SEARCH3}/${REPLACE3}/g" values.yaml
  
  
- sed -i "s/${SEARCH1}/${REPLACE1}/g" values1.yaml
- sed -i "s/${SEARCH2}/${REPLACE2}/g" values1.yaml
+ #sed -i "s/${SEARCH1}/${REPLACE1}/g" values1.yaml
+ #sed -i "s/${SEARCH2}/${REPLACE2}/g" values1.yaml
  #sed -i "s/${SEARCH3}/${REPLACE3}/g" values1.yaml
  
- sed -i 's/secretName: ""/secretName: "Harbor"/g' values.yaml
+ #sed -i 's/secretName: ""/secretName: "Harbor"/g' values.yaml
  
  helm install harbor . -n harbor
  
