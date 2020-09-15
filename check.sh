8c9ff2095cfd2874c1b4ebb532aa3d458ee16de8
Sakshi Rathore <Sakshi.Rathore@bgw-online.de>
2020-09-15 09:43:02 +0000

    commiting the new file

--- check.sh
+++ check.sh
@@ -4,10 +4,10 @@ echo "namespace is $NAMESPACE_OUTPUT"
 if [ -z "$NAMESPACE_OUTPUT" ]
 
 then
- #     echo  " "$NAMESPACE_OUTPUT" is NULL"
-#	  chmod +x new_server_cert.sh
+     echo  " "$NAMESPACE_OUTPUT" is NULL"
+	  chmod +x new_server_cert.sh
  #   ./new_server_cert.sh
-#else
+else
      echo " "$NAMESPACE_OUTPUT" is NOT NULL"
 	 chmod +x cert_status1.sh
     ./cert_status1.sh
