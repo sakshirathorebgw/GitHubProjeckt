d649c19977da60b9e7528c49658a695f22d97d51
Sakshi Rathore <Sakshi.Rathore@bgw-online.de>
2020-09-15 10:00:02 +0000

    commiting the new file

--- piecebypiece.sh
+++ piecebypiece.sh
@@ -136,6 +136,30 @@ done
 
 for server in $(cat server1.txt)
 do
+	scp $CERT_PATH ccpuser@$server:/home/ccpuser
+scp $KEY_FILE ccpuser@$server:/home/ccpuser
+scp $CRT_FILE ccpuser@$server:/home/ccpuser
+scp ./check.sh ccpuser@$server:/home/ccpuser
+scp ./old_server.sh ccpuser@$server:/home/ccpuser
+scp ./cert_status1.sh ccpuser@$server:/home/ccpuser
+scp ./new_server_cert.sh ccpuser@$server:/home/ccpuser
+scp ./server.txt ccpuser@$server:/home/ccpuser
+scp ./server1.txt ccpuser@$server:/home/ccpuser
+scp ./cert_update.sh ccpuser@$server:/home/ccpuser
+scp ./inputfile.txt ccpuser@$server:/home/ccpuser
+scp ./harbor.zip ccpuser@$server:/home/ccpuser
+
+sed -i -e 's/\r$//' ./check.sh
+sed -i -e 's/\r$//' ./old_server.sh
+sed -i -e 's/\r$//'  ./cert_status1.sh
+sed -i -e 's/\r$//' ./old_server.sh
+sed -i -e 's/\r$//' ./new_server_cert.sh
+sed -i -e 's/\r$//' server.txt
+sed -i -e 's/\r$//' server1.txt
+sed -i -e 's/\r$//' ./cert_update.sh
+sed -i -e 's/\r$//' inputfile.txt
+sed -i -e 's/\r$//' harbor.zip
+
 	echo $server
 #	ssh ccpuser@$server /bin/bash <<EOF
 ls
