60d187f0ebde35f9fc85be37bd1966f04c45a5ca
Sakshi Rathore <Sakshi.Rathore@bgw-online.de>
2020-09-24 08:17:50 +0000

    commiting the new file

--- old_server.sh
+++ old_server.sh
@@ -99,7 +99,7 @@ echo "crt filename is" $CRT_FILENAME
  cat ca.crt |base64 | tr -d "\n" > ca.crt.txt
  sed -i '1s/^/  ca.crt: /' ca.crt.txt
 
- kubectl create secret tls $TLS_NAME --key $KEY_FILENAME --cert $CRT_FILENAME --cert ca.crt --namespace harbor
+ kubectl create secret tls $TLS_NAME --key $KEY_FILENAME --cert $CRT_FILENAME --namespace harbor
  kubectl get secret $TLS_NAME -n harbor -o yaml > ca.crt.yaml
 
  sed -i '/data:/r ca.crt.txt' ca.crt.yaml
