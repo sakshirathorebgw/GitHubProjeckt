cf4fcc44f5a6396de5d8ca1bbb32c8152c836252
Sakshi Rathore <Sakshi.Rathore@bgw-online.de>
2020-09-29 14:07:29 +0000

    commiting the new file

--- old_server.sh
+++ old_server.sh
@@ -111,6 +111,20 @@ echo "crt filename is" $CRT_FILENAME
 
   CN_ADDRESS=$(openssl x509 -noout -subject -in $CRT_FILENAME | awk '{ print $18 }' | sed 's/.$//' 2>&1)
    
+cat ca.crt |base64 | tr -d "\n" > ca.crt.txt
+ sed -i '1s/^/  ca.crt: /' ca.crt.txt
+ sed -i 's/$/\n/' ca.crt.txt
+ kubectl create secret tls $TLS_NAME --key $KEY_FILENAME --cert $CRT_FILENAME --namespace harbor
+ kubectl get secret $TLS_NAME -n harbor -o yaml > ca.crt.yaml
+
+ #sed -i '/data:/r ca.crt.txt' ca.crt.yaml
+ awk 'FNR==3{system("cat ca.crt.txt")} 1' ca.crt.yaml > ca.crt_final.yaml
+
+ kubectl delete $TLS_NAME -n harbor
+#kubectl apply -f ca.crt_final.yaml
+ kubectl apply -f ca.crt_final.yaml
+
+
   SEARCH1="core: mgmt-registry.tank.local"
       #  echo $SEARCH1
         REPLACE1="core: $CN_ADDRESS"
@@ -138,18 +152,7 @@ echo "crt filename is" $CRT_FILENAME
  #echo $output
  #######cat root_ca.crt |base64 | tr -d "\n"
 
- #cat ca.crt |base64 | tr -d "\n" > ca.crt.txt
- #sed -i '1s/^/  ca.crt: /' ca.crt.txt
- #sed -i 's/$/\n/' ca.crt.txt
- kubectl create secret tls $TLS_NAME --key $KEY_FILENAME --cert $CRT_FILENAME --namespace harbor
- #kubectl get secret $TLS_NAME -n harbor -o yaml > ca.crt.yaml
 
- #sed -i '/data:/r ca.crt.txt' ca.crt.yaml
- #awk 'FNR==3{system("cat ca.crt.txt")} 1' ca.crt.yaml > ca.crt_final.yaml
-
- #kubectl delete $TLS_NAME -n harbor 
-#kubectl apply -f ca.crt_final.yaml
- #kubectl apply -f ca.crt.yaml
 
 # helm install harbor . -n harbor
 helm upgrade harbor . -n harbor 
