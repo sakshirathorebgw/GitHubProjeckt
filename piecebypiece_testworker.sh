ffb41f1a92df28245c903843f37ba3ae74de1a09
Sakshi Rathore <Sakshi.Rathore@bgw-online.de>
2020-09-29 13:24:40 +0000

    commiting the new file

--- piecebypiece_testworker.sh
+++ piecebypiece_testworker.sh
@@ -180,7 +180,7 @@ scp ./server.txt ccpuser@$server:/home/ccpuser
 scp ./server1.txt ccpuser@$server:/home/ccpuser
 scp ./cert_update.sh ccpuser@$server:/home/ccpuser
 scp ./inputfile.txt ccpuser@$server:/home/ccpuser
-scp ./harbor.zip ccpuser@$server:/home/ccpuser
+scp harbor ccpuser@$server:/home/ccpuser
 scp $DIR/ca.crt ccpuser@$server:/home/ccpuser
 
 sed -i -e 's/\r$//' ./check.sh
