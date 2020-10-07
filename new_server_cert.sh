e0b8ddc8199da6ce0aa80630fd767296cf65af00
Sakshi Rathore <Sakshi.Rathore@bgw-online.de>
2020-10-07 12:21:18 +0000

    commiting the new file

--- new_server_cert.sh
+++ new_server_cert.sh
@@ -144,14 +144,14 @@ sleep 5
 		REPLACE3="secretName: "\"$TLS_NAME\"""
                 echo $REPLACE3
  #sed -i "s/secretName:tank/${REPLACE3}/g" values.yaml
-  sed -i "s/secretName:.*/${REPLACE3}/g" values.yaml
-  sed -i "0,/ secretName: /{s/ secretName:.*/ ${REPLACE3}/}" values.yaml
+ # sed -i "s/secretName:.*/${REPLACE3}/g" values.yaml
+  sed -i "0,/ secretName: /{s/secretName:.*/ ${REPLACE3}/}" values.yaml
 
   HTTPS_STRING="https://"
   REPLACE4="externalURL: $HTTPS_STRING$CN_ADDRESS"
   #REPLACE2="externalURL: https://"\$CN_ADDRESS""
-         echo $REPLACE2
- sed -i "0,/ externalURL: /{s/ externalURL:.*/ ${REPLACE4}/}" values.yaml
+         echo $REPLACE4
+ sed -i "0,/externalURL: /{s/ externalURL:.*/ ${REPLACE4}/}" values.yaml
 
  echo "values.yaml is updated"
 
