453ac44043c36706f4c082c747290bf2d267d25f
Sakshi Rathore <Sakshi.Rathore@bgw-online.de>
2020-10-28 13:36:31 +0000

    commiting the new file

--- gitpush_97.sh
+++ gitpush_97.sh
@@ -14,9 +14,10 @@ chmod 600 ~/.ssh/*
 
 #scp $SSH_KEYPATH ccpuser@$HOST_NAME:/home/ccpuser/.ssh
 
+./ip_fetch.sh
 
 git config --global user.email "Sakshi.Rathore@bgw-online.de"
-git config --global user.name "Sakshi Rathore"
+git config --global user.name "Administrator"
 git status
 git add .
 
@@ -27,11 +28,14 @@ echo $commitMessage
 
 #git commit -m "$commitMessage"
 #git branch -mv origin master
-git remote set-url origin https://Sakshi.Rathore%40bgw-online.de:Sherlock%40107@github.com/sakshirathorebgw/GitHubProjeckt.git
+#git remote set-url origin https://Sakshi.Rathore%40bgw-online.de:Sherlock%40107@github.com/sakshirathorebgw/GitHubProjeckt.git
+git remote add origin http://root:Gitlab123@harbortenant-master-gro-63b466fab2/root/GitHubProjeckt.git
 #git remote set-url https://github.com/sakshirathorebgw/GitHubProjeckt.git 
 #branch=master
 #echo $branch
 #read branch
+#sudo git config --system --unset credential.helper
+sudo git config --global http.emptyAuth true
 
 git commit -m "$commitMessage"
 
@@ -46,4 +50,4 @@ echo "execute piecebypiece now"
 #./piecebypiece.sh
 #piecebypiece_install.sh
 #./piecebypiece_testworker.sh
-#echo "script execution done"
+echo "script execution done"
